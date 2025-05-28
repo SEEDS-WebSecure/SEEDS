8d4b690bc921c4b8d5669301aaaf50fc477ad10d
mk-star <ssunn0812@gmail.com>
2025-05-28 09:08:36 +0900

    feat: 키 생성 메뉴 따로 분리

--- src/main/java/com/websecure/seeds/controller/HomeController.java
+++ src/main/java/com/websecure/seeds/controller/HomeController.java
@@ -1,12 +1,15 @@
 package com.websecure.seeds.controller;
 
+import com.websecure.seeds.dto.CreateKeyDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("createKeyDTO", new CreateKeyDTO());
         return "createKeyForm";
     }
 }
